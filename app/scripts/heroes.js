f4605e3396d30e9e7205db7e762dd078422eaab1
Todd Heidenreich <jtheidenreich@gmail.com>
2015-05-17 15:14:15 -0400

    initial commit

---
+++ app/scripts/heroes.js
@@ -0,0 +1,2 @@
+charactersJSON = $.get("http://gateway.marvel.com/v1/public/characters?orderBy=name&apikey=c9f2153bf7d95a2efc7709ecafe33582");
+seriesJSON = $.get("http://gateway.marvel.com/v1/public/series?apikey=c9f2153bf7d95a2efc7709ecafe33582")
